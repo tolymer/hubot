3e1c0091435ae42418b63d52e26e324ddf86fa10
1000ch <shogo.sensui@gmail.com>
2016-10-19 01:11:02 +0900

    JSify mahjong

---
+++ scripts/mahjong.js
@@ -0,0 +1,48 @@
+// Description:
+//   mahjong
+//
+// Commands:
+//   hubot mahjong haipai
+
+const Mahjong = require('../models/mahjong');
+
+module.exports = (robot) => {
+  robot.respond(/mahj[oa]ng (.*)$/i, (msg) => {
+    let { type, pai } = Mahjong.parseCommand(msg.match[1]);
+
+    const restore = () => {
+      let { pais, discardedPais, doraDisplayedPai } = robot.brain.get('mahjong');
+      mahjong = new Mahjong(pais, discardedPais, doraDisplayedPai);
+    };
+
+    if (process.env['DEBUG']) {
+      console.log(type, pai);
+    }
+
+    switch (type) {
+      case Mahjong.HAIPAI:
+        mahjong = new Mahjong().haipai();
+        break;
+      case Mahjong.TSUMOGIRI:
+        mahjong = restore();
+        mahjong.tsumogiri()
+        break;
+      case Mahjong.DISCARD:
+        mahjong = restore();
+        if (!mahjong.discard(pai)) {
+          return msg.send('チョンボ');
+        }
+        break;
+      default:
+        msg.send('チョンボ');
+        return;
+    }
+
+    msg.send mahjong.display();
+    robot.brain.set('mahjong', {
+      pais: mahjong.pais,
+      discardedPais: mahjong.discardedPais,
+      doraDisplayedPai: mahjong.doraDisplayedPai,
+    });
+  });
+});
