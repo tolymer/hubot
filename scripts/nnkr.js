51a152be3d2471fdd5f6a501f6fbed66a51c6c0f
1000ch <shogo.sensui@gmail.com>
2016-10-19 12:14:36 +0900

    Add eslint integration

--- scripts/nnkr.js
+++ scripts/nnkr.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 module.exports = (robot) => {
   robot.respond(/nnkr/i, (msg) => {
-    cp.exec(`${phantomjs.path} ${path.join(__dirname, '../nnkr-request.js')}`, => {
+    cp.exec(`${phantomjs.path} ${path.join(__dirname, '../nnkr-request.js')}`, () => {
       let nnkr = path.join(__dirname, '..', 'nnkr.png');
       let room = msg.message.room;
       let token = process.env.HUBOT_SLACK_TOKEN;
