fad6377a07d24d6a92d427bff10531d6965d10b1
1000ch <shogo.sensui@gmail.com>
2016-10-20 21:24:09 +0900

    Optimize png

--- scripts/nnkr.js
+++ scripts/nnkr.js
@@ -5,17 +5,23 @@
 //   hubot nnkr
 
 const phantomjs = require('phantomjs');
-const cp = require('child_process');
 const path = require('path');
+const pify = require('pify');
+const imgo = require('imgo');
+const cpP = pify(require('child_process'));
+const fsP = pify(require('fs'));
 
 module.exports = (robot) => {
   robot.respond(/nnkr/i, (msg) => {
-    cp.exec(`${phantomjs.path} ${path.join(__dirname, '../nnkr-request.js')}`, () => {
-      let nnkr = path.join(__dirname, '..', 'nnkr.png');
-      let room = msg.message.room;
-      let token = process.env.HUBOT_SLACK_TOKEN;
-      let url = 'https://slack.com/api/files.upload';
-      cp.exec(`curl -F file=@${nnkr} -F channels=${room} -F token=${token} ${url}`);
-    });
+    let nnkr = path.join(__dirname, '..', 'nnkr.png');
+    let room = msg.message.room;
+    let token = process.env.HUBOT_SLACK_TOKEN;
+    let url = 'https://slack.com/api/files.upload';
+
+    cpP.exec(`${phantomjs.path} ${path.join(__dirname, '../nnkr-request.js')}`)
+      .then(() => fsP.readFile(nnkr))
+      .then(buffer => imgo(buffer, { pngquant : true }))
+      .then(buffer => fsP.writeFile(nnkr, buffer))
+      .then(() => cpP.exec(`curl -F file=@${nnkr} -F channels=${room} -F token=${token} ${url}`));
   });
 };
