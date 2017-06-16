cd24aa035a1b3c7195a3030cae27971a108b9787
Kazuhito Hokamura <k.hokamura@gmail.com>
2017-06-16 14:45:35 +0900

    Clip image

--- scripts/nnkr.js
+++ scripts/nnkr.js
@@ -12,16 +12,18 @@ const fs = pify(require('fs'));
 
 module.exports = (robot) => {
   robot.respond(/nnkr/i, (msg) => {
-    let nnkr = path.join(__dirname, '..', 'screenshot.png');
+    let screenshot = path.join(__dirname, '..', 'screenshot.png');
+    let out = path.join(__dirname, '..', 'out.png');
     let room = msg.message.room;
     let token = process.env.HUBOT_SLACK_TOKEN;
     let url = 'https://slack.com/api/files.upload';
 
-    cp.exec(`${process.env.GOOGLE_CHROME_BIN} --headless --no-sandbox --disable-gpu --screenshot http://nnkr.jp/`)
-      .then(() => fs.readFile(nnkr))
+    cp.exec(`${process.env.GOOGLE_CHROME_BIN} --headless --no-sandbox --disable-gpu --window-size=1280,640 --screenshot http://nnkr.jp/`)
+      .then(() => cp.exec(`convert -crop 740x130+270+255 ${screenshot} ${out}`))
+      .then(() => fs.readFile(out))
       .then(buffer => imgo(buffer, { pngquant : true }))
-      .then(buffer => fs.writeFile(nnkr, buffer))
-      .then(() => cp.exec(`curl -F file=@${nnkr} -F channels=${room} -F token=${token} ${url}`))
+      .then(buffer => fs.writeFile(out, buffer))
+      .then(() => cp.exec(`curl -F file=@${out} -F channels=${room} -F token=${token} ${url}`))
       .catch(error => console.error(error));
   });
 };
